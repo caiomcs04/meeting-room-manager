e60906b453a861520ffd9d445589b60d02debc01
caiomcs04 <caio6c@yahoo.com.br>
2021-07-15 08:17:59 -0300

    3#Commit(Exceptions)

--- src/main/java/com/digital/crud/meetingroom/meetingroom/exception/GlobalExceptionHandle.java
+++ src/main/java/com/digital/crud/meetingroom/meetingroom/exception/GlobalExceptionHandle.java
@@ -10,7 +10,7 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GlobalExceptionHandle {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> resourseNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
