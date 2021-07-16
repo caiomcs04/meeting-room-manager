06636da1d5c7b84fe6286c17df18e489f24f3f19
caiomcs04 <caio6c@yahoo.com.br>
2021-07-16 13:35:21 -0300

    6#(Angular init + folder relocation)

--- src/main/java/com/digital/crud/meetingroom/meetingroom/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.digital.crud.meetingroom.meetingroom.repository;
-
-import com.digital.crud.meetingroom.meetingroom.model.Room;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, Long>{
-}
