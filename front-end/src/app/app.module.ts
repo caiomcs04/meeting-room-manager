8a1f8cddc308e7d8c4131b4c48769ef1eed36b84
caiomcs04 <caio6c@yahoo.com.br>
2021-07-18 16:08:41 -0300

    11#commit(pipe de data)

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,13 @@ import { RoomDetailsComponent } from './room-details/room-details.component';
 import { RoomListComponent } from './room-list/room-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UpdateRoomComponent } from './update-room/update-room.component';
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+
+/*Esse methodo do angular serve para fazer a localização da data.
+necessário importar tanto o metodo quando a variavel localePt*/
+registerLocaleData(localePt, 'pt')
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +30,10 @@ import { UpdateRoomComponent } from './update-room/update-room.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    /*Aqui em providers será declarada as localizações dos pipes do app*/
+    {provide: LOCALE_ID, useValue: 'pt'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
